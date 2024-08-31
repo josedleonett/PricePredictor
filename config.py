d9e3e344fceaedd97baa294168e0c43b8c5d5ad7
JosedLeonett <josed.95@gmail.com>
2024-08-31 10:33:37 -0300

    Agregado la correccion automatica de fechas

--- config.py
+++ config.py
@@ -1,26 +1,18 @@
-# Crear un archivo de configuración para almacenar las variables de entorno
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # Variables de entorno 
 
-BASE_API_URL = os.getenv("PP_BASE_API_URL")
-BASE_API_TOKEN = os.getenv("PP_BASE_API_TOKEN")
-
-print(BASE_API_URL, BASE_API_TOKEN)
+BASE_API_URL = "https://api.estadisticasbcra.com"
+# BASE_API_TOKEN = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTYyNjEzMzgsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJqb3NlZC45NUBnbWFpbC5jb20ifQ.UG71DSBLw2V6qg7NjxlsbGe0FSOmApXXv16saNZi3SHNM63nkWahuZi94AXdpJWJbmUAzoUVFLC8yImq6paOPw"
+BASE_API_TOKEN = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTY2Mzg0NDEsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJqbGVvbmV0dEBlY29ub3ZvLmNvbS5hciJ9.v38WYW2ur5PLWU4MO1de1dQYdjJRX1A0X5P-b34a0ebmAW-_g9YkR2lZ4O4baSMkH2a4EgvjB1VQuy1A6QaaqA"
 
 # Variables de entorno para la API de DolarBlue
-DOLAR_BLUE_API_URL = os.getenv("DOLAR_BLUE_API_URL")
-DOLAR_BLUE_API_TOKEN = os.getenv("DOLAR_BLUE_API_TOKEN")
+DOLAR_BLUE_API_URL = BASE_API_URL + "/usd"
+DOLAR_BLUE_API_TOKEN = BASE_API_TOKEN
 
 # Variables de entorno para la API de DolarOficial
-DOLAR_OFICIAL_API_URL = os.getenv("DOLAR_OFICIAL_API_URL")
-DOLAR_OFICIAL_API_TOKEN = os.getenv("DOLAR_OFICIAL_API_TOKEN")
+DOLAR_OFICIAL_API_URL = BASE_API_URL + "/usd_of"
+DOLAR_OFICIAL_API_TOKEN = BASE_API_TOKEN
 
 # Variables de entorno para la API de la InflacionMensualOficial
-INFLACION_OFICIAL_API_URL = os.getenv("INFLACION_OFICIAL_API_URL")
-INFLACION_OFICIAL_API_TOKEN = os.getenv("INFLACION_OFICIAL_API_TOKEN")
+INFLACION_OFICIAL_API_URL = BASE_API_URL + "/inflacion_mensual_oficial"
+INFLACION_OFICIAL_API_TOKEN = BASE_API_TOKEN
 
