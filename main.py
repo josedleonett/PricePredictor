04533a613e5c8e9cce225b992d928dd908890cb0
JosedLeonett <josed.95@gmail.com>
2024-08-31 12:39:34 -0300

    Agregado el conversor a datos mensuales

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-from utils.data_needs_update import data_needs_update
-from utils.update_data import update_data
-from utils.verify_and_normalize_dates import verify_and_normalize_dates
-from utils.convert_to_monthly_data import convert_to_monthly_data
+from utils.get_and_preprocess_data import get_and_preprocess_data
 
-
-# Verificar si los datos necesitan ser actualizados
-if data_needs_update("temp/raw_data/online"):
-    update_data("temp/raw_data/online")
-
-# Verifica si las fechas esta normalizadas
-verify_and_normalize_dates("temp/raw_data/online")
-verify_and_normalize_dates("temp/raw_data/offline")
-
-# Verifica y preprocesa los datos dejando solo los datos mensuales
-convert_to_monthly_data("temp/raw_data/online", "temp/pre-processed")
-convert_to_monthly_data("temp/raw_data/offline", "temp/pre-processed")
+get_and_preprocess_data()
---
+++ utils/get_and_preprocess_data.py
@@ -0,0 +1,18 @@
+from utils.data_needs_update import data_needs_update
+from utils.update_data import update_data
+from utils.verify_and_normalize_dates import verify_and_normalize_dates
+from utils.convert_to_monthly_data import convert_to_monthly_data
+
+
+# Verificar si los datos necesitan ser actualizados
+def get_and_preprocess_data():
+    if data_needs_update("temp/raw_data/online"):
+        update_data("temp/raw_data/online")
+
+    # Verifica si las fechas esta normalizadas
+    verify_and_normalize_dates("temp/raw_data/online")
+    verify_and_normalize_dates("temp/raw_data/offline")
+
+    # Verifica y preprocesa los datos dejando solo los datos mensuales
+    convert_to_monthly_data("temp/raw_data/online", "temp/pre-processed")
+    convert_to_monthly_data("temp/raw_data/offline", "temp/pre-processed")
